bbf05bc991e819b639d9a5fddaef76e67838298f
hisense <lizhixin4@hisense.com>
2020-04-24 16:03:35 +0800

    no message

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 // const REQUEST_CONFIG = 
 window.appConfig = {
-    baseUrl: 'https://xwj.juhaolian.cn/',      //微服务地址  
+    baseUrl: 'https://test99xwj.juhaolian.cn/',      //微服务地址  
     openAppUrl:"https://testxwj.juhaolian.cn/shareToApp-owner-vue/" ,//打开App跳转链接
     androidOpenUrl:"juhaolian://testxwj.juhaolian.cn/shareToApp-owner-vue/",
     versionInfo:'V3.2.0.0',
