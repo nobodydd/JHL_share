d74a974f8e15c8c7f88c1f5482b416b1f1c159f2
hisense <lizhixin4@hisense.com>
2020-04-28 17:35:41 +0800

    地方

--- app.config.js
+++ app.config.js
@@ -1,7 +1,14 @@
 // const REQUEST_CONFIG = 
 window.appConfig = {
+
+    //部署版本时,运维只需要修改这个地址即可
     baseUrl: 'https://test99xwj.juhaolian.cn/',      //微服务地址  
-    openAppUrl:"https://testxwj.juhaolian.cn/shareToApp-owner-vue/" ,//打开App跳转链接
-    androidOpenUrl:"juhaolian://testxwj.juhaolian.cn/shareToApp-owner-vue/",
-    versionInfo:'V3.2.0.0',
+
+
+
+    //========================以下地址长期不变,部署时暂时不用修改=======================================
+    //========================以下地址长期不变,部署时暂时不用修改=======================================
+    openAppUrl: "https://testxwj.juhaolian.cn/shareToApp-owner-vue/",//打开App跳转链接
+    androidOpenUrl: "juhaolian://testxwj.juhaolian.cn/shareToApp-owner-vue/",
+    versionInfo: 'V3.2.0.0',
 }
