bae526f93400691d7dc459a1fe6dbde099895583
hisense <lizhixin4@hisense.com>
2020-06-05 16:54:32 +0800

    no message

---
+++ app.config.js
@@ -0,0 +1,15 @@
+// const REQUEST_CONFIG = 
+window.appConfig = {
+
+    //部署版本时,运维只需要修改这个地址即可
+    baseUrl: 'http://219.147.31.38:8126/',      //微服务地址  
+
+
+
+    //========================以下地址长期不变,部署时暂时不用修改=======================================
+    //========================以下地址长期不变,部署时暂时不用修改=======================================
+    openAppUrlForiOSTest: "https://testxwj.juhaolian.cn/shareToApp-owner-vue/",//打开App跳转链接,test环境
+    openAppUrlForiOSProd: "https://xwj.juhaolian.cn/shareToApp-owner-vue/",//打开App跳转链接,正式环境
+    androidOpenUrl: "juhaolian://testxwj.juhaolian.cn/shareToApp-owner-vue/",
+    versionInfo: 'V3.2.0.0',
+}
