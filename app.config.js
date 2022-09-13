037447c86731dc0af06e11116533978ce7987658
lizhixin <lizhixin4@hisense.com>
2022-09-13 15:39:25 +0800

    爱家test

---
+++ app.config.js
@@ -0,0 +1,9 @@
+// window.appConfig = { 
+//     // 如果需要其他接口， 命名一个非baseUrl的地址， 在server文件夹中根据接口js文件， 添加get/post方法， 依据http.js格式进行编码
+//     baseUrl: 'https://testxwj.juhaolian.cn',            //微服务地址
+//     //hisenseUrl: 'http://m.test.shop.hisense.com',     //微服务地址  海信商城测试环境
+//     hisenseUrl: 'http://m.hisense.com',                 //微服务地址  海信商城生产环境
+//     lvwangUrl:'http://api.open.lvwang.org.cn',
+//     testEnvUrl: 'http://api.wg.hismarttv.com/'
+//     // WebSocket:'ws',//推送消息模块协议                 // 生产环境应将ws调整为wss
+// }
